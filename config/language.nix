61bc9925a3581c2a3ffc4c20bd563d15523a99ba
Anthony J Mirabella <a9@aneurysm9.com>
2026-04-15 21:13:46 -0400

    fix: update flake inputs and replace deprecated vim.lsp.with()
    vim.lsp.with() was removed in neovim 0.12. Use vim.o.winborder for
    global floating window border configuration instead.
    
    Signed-off-by: Anthony J Mirabella <a9@aneurysm9.com>

--- config/language.nix
+++ config/language.nix
@@ -221,15 +221,7 @@ in
             },
           })
 
-          vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-            vim.lsp.handlers.hover,
-            {border = 'rounded'}
-          )
-
-          vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-            vim.lsp.handlers.signature_help,
-            {border = 'rounded'}
-          )
+          vim.o.winborder = 'rounded'
         '';
         postConfig = ''
           local levels = { ERROR = " ", WARN = " ", HINT = " ", INFO = " " }
